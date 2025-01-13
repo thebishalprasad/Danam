cd3b730c85bb5967b8edfac5edd0f3129902a2e8
Bishal Prasad <bishal.prasad3787@gmail.com>
2025-01-14 00:09:17 +0530

    Fixed Extension

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import HomeHero from './components/HomeHero';
-import HomeAbout from './components/HomeAbout';
-import DonateForm from './components/DonateForm';
+import HomeHero from './components/HomeHero.jsx';
+import HomeAbout from './components/HomeAbout.jsx';
+import DonateForm from './components/DonateForm.jsx';
 
 const Home = () => {
   return (
